642be2e6108a56d6c7e3e0c73d68a54f0a2c3085
unknown <event@MSOE-R90YB6A5.ad.msoe.edu>
2022-03-09 16:19:38 -0600

    begin loading

---
+++ src/main/java/frc/robot/commands/LoadCommand.java
@@ -0,0 +1,9 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.LoadSubsystem;
+
+public class LoadCommand {
+    
+}
---
+++ src/main/java/frc/robot/subsystems/LoadSubsystem.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+
+public class LoadSubsystem extends SubsystemBase {
+    
+}
