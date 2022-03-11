74aef112caa01458db8f02151b4cc773ad39e098
Doughty <doughtyc@msoe.edu>
2022-03-11 07:52:21 -0600

    Shooter speed modifications

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -66,8 +66,8 @@ public final class Constants {
         }
 
         public final class shooter {
-            public static final double TOP_SPEED = 1.0;
-            public static final double BOTTOM_SPEED = -1.0;
+            public static final double TOP_SPEED = -0.9;
+            public static final double BOTTOM_SPEED = 0.9;
             public static final double TOP_WRONG_BALL_SPEED = 0.1;
             public static final double BOTTOM_WRONG_BALL_SPEED = -0.1;
         }
