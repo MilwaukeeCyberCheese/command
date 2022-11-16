293ae4c69bd9d6ecefb48ea5b60ccf41aa3b357c
Sam Freund <techguy763@gmail.com>
2022-11-15 20:34:08 -0600

    VSCode said I should, so I did

--- src/main/java/frc/robot/commands/EjectCommand.java
+++ src/main/java/frc/robot/commands/EjectCommand.java
@@ -2,7 +2,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import frc.robot.Robot;
 import frc.robot.other.Stopwatch;
 import frc.robot.subsystems.ShooterSubsystem;
 
