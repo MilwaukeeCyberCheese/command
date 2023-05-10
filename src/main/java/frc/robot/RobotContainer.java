b67805f23f1ec76dfa94210ca1563f140515a103
Sam <techguy763@gmail.com>
2023-05-10 14:03:49 +0000

    refactor to new command method

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.AutoCommand;
 import frc.robot.commands.DriveCommand;
 import frc.robot.commands.IntakeCommand;
@@ -40,39 +41,27 @@ public class RobotContainer {
 
   // the robot's subsystems and commands are defined here...
   public static final DriveSubsystem m_driveSubsystem = new DriveSubsystem();
-  public static final DriveCommand m_driveCommand = new DriveCommand(
-      m_driveSubsystem,
-      () -> (modifyAxis(filteredController.getYLeft(.2)) * DriveSubsystem.MAX_VELOCITY_METERS_PER_SECOND)
-          * Constants.subsystems.drive.strafe,
-      () -> (-modifyAxis(filteredController.getXLeft(.2)) * DriveSubsystem.MAX_VELOCITY_METERS_PER_SECOND)
-          * Constants.subsystems.drive.strafe,
-      () -> (modifyAxis(filteredController.getXRight(.2)) * -DriveSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND)
-          * Constants.subsystems.drive.turnRate);
 
   public static final AutoSubsystem m_autoSubsystem = new AutoSubsystem();
   private static final AutoCommand m_autoCommand = new AutoCommand(m_autoSubsystem);
 
   private static final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
-  private static final IntakeCommand m_intakeCommand = new IntakeCommand(m_intakeSubsystem);
-  private static final OuttakeCommand m_outtakeCommand = new OuttakeCommand(m_intakeSubsystem);
-  // private static final OuttakeSubsystem m_outtakeSubsystem = new
-  // OuttakeSubsystem();
-  // private static final OuttakeCommand m_outtakeCommand = new
-  // OuttakeCommand(m_intakeSubsystem);
 
   private static final ServoSubsystem m_servoSubsystem = new ServoSubsystem();
-  private static final ServoCommand m_servoCommand = new ServoCommand(m_servoSubsystem);
 
   private static final ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem();
-  private static final HighShooterCommand m_highShooterCommand = new HighShooterCommand(m_shooterSubsystem, m_servoSubsystem);
-  private static final LowShooterCommand m_lowShooterCommand = new LowShooterCommand(m_shooterSubsystem, m_servoSubsystem);
-  private static final EjectCommand m_ejectCommand = new EjectCommand(m_shooterSubsystem);
-
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
-    m_driveSubsystem.setDefaultCommand(m_driveCommand);
+    m_driveSubsystem.setDefaultCommand(new DriveCommand(
+        m_driveSubsystem,
+        () -> (modifyAxis(filteredController.getYLeft(.2)) * DriveSubsystem.MAX_VELOCITY_METERS_PER_SECOND)
+            * Constants.subsystems.drive.strafe,
+        () -> (-modifyAxis(filteredController.getXLeft(.2)) * DriveSubsystem.MAX_VELOCITY_METERS_PER_SECOND)
+            * Constants.subsystems.drive.strafe,
+        () -> (modifyAxis(filteredController.getXRight(.2)) * -DriveSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND)
+            * Constants.subsystems.drive.turnRate));
 
     // Configure the button bindings
     configureButtonBindings();
@@ -95,13 +84,13 @@ public class RobotContainer {
    * it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    new Button(filteredController::getLeftTriggerActive).whileHeld(m_intakeCommand);
-    new Button(filteredController::getLeftBumper).whileHeld(m_outtakeCommand);
-    new Button(filteredController::getRightTriggerActive).whileHeld(m_highShooterCommand);
-    new Button(filteredController::getRightBumper).whileHeld(m_lowShooterCommand);
-    new Button(filteredController::getAButton).whileHeld(m_servoCommand);
-    new Button(filteredController::getYButton).whileHeld(m_ejectCommand);
-    new Button(filteredController::getPOVPressed).whenActive(new Runnable() {
+    new Trigger(filteredController::getLeftTriggerActive).whileTrue(new IntakeCommand(m_intakeSubsystem));
+    new Trigger(filteredController::getLeftBumper).whileTrue(new OuttakeCommand(m_intakeSubsystem));
+    new Trigger(filteredController::getRightTriggerActive).whileTrue(new HighShooterCommand(m_shooterSubsystem, m_servoSubsystem));
+    new Trigger(filteredController::getRightBumper).whileTrue(new LowShooterCommand(m_shooterSubsystem, m_servoSubsystem));
+    new Trigger(filteredController::getAButton).whileTrue( new ServoCommand(m_servoSubsystem));
+    new Trigger(filteredController::getYButton).whileTrue(new EjectCommand(m_shooterSubsystem));
+    new Trigger(filteredController::getPOVPressed).whenActive(new Runnable() {
       @Override
       public void run() {
         if (!readAuto) {
